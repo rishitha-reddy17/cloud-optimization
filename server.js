f514d356d5538d3b0e0cd10fa157de62b9274dad
rishitha-reddy17 <rishithareddyaleti802@gmail.com>
2026-04-17 17:45:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -75,4 +75,4 @@ app.post("/connect-azure", async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Backend running on http:// localhost:3000"));
+app.listen(3000, () => console.log("Backend running on http://localhost:3000"));
