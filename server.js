ebd3648801b1cd15d4188c1b3a8695aeac0c392d
rishitha-reddy17 <rishithareddyaleti802@gmail.com>
2026-04-17 17:28:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
+ 
 // 🔐 Your credentials will come from frontend
 app.post("/connect-azure", async (req, res) => {
     const { tenantId, clientId, clientSecret, subscriptionId } = req.body;
